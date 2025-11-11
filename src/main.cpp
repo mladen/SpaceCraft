2e925f30446be7f76dbb06cc579ef32bd5c9ff99
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2025-11-11 02:54:46 +0100

    Makes a rectangle using two triangles (uses vertices and indices)

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Use the core profile of OpenGL
 
     // Create a GLFW window with the specified dimensions and title
-    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Spacecraft", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "SpaceCraft", NULL, NULL);
     if (!window) // Checks if the window was successfully created
     {
         std::cerr << "Failed to create GLFW window" << std::endl;
@@ -73,25 +73,33 @@ int main()
 
     // Set up vertex data (and buffer(s)) and configure vertex attributes
     float vertices[] = {
-        // Rectangle
-        0.5f, 0.5f, 0.0f,   // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f, 0.5f, 0.0f,  // top left
+        // Triangle 1
+        0.5f,
+        0.5f,
+        0.0f, // top right
+        -0.5f,
+        0.5f,
+        0.0f, // top left
+        -0.5f,
+        -0.5f,
+        0.0f, // bottom left
 
-        // Triangle
-        0.6f, 0.6f, 0.0f,
-        0.6f, -0.6f, 0.0f,
-        0.9f, 0.0f, 0.0f};
+        // Triangle 2
+        // 0.0f, 0.0f, 0.0f,
+        0.5f,
+        -0.5f,
+        0.0f,
+        // 0.5f, 0.0f, 0.0f
+    };
 
     // Set up index data; This is used to specify which vertices make up each triangle
     unsigned int indices[] = {
-        0, 1, 3,  // first triangle
-        1, 2, 3,  // second triangle
-        4, 5, 6}; // third triangle
+        0, 1, 2,  // first triangle; an indice of 0 refers to the first vertex (triplet of coordinates - (0.5, 0.5, 0.0) in our case)
+        0, 2, 3}; // second triangle
 
     // Generate and configure buffers
-    unsigned int VAO, VBO, EBO;
+    unsigned int VAO,
+        VBO, EBO;
     glGenVertexArrays(1, &VAO); // VAO = Vertex Array Object; stores the state of the vertex attribute configuration and uses it when rendering
     glGenBuffers(1, &VBO);      // VBO = Vertex Buffer Object; stores the vertex data
     glGenBuffers(1, &EBO);      // EBO = Element Buffer Object; stores the index data
